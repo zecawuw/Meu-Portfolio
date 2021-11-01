f6697f73c752a18e0404cb4c742813b1e5097571
zeca <joaopedrosantosdonascimentodev@gmail.com>
2021-11-01 20:40:04 -0300

    meu portifolio

---
+++ script.js
@@ -0,0 +1,79 @@
+// popup1
+
+function certificado_aplicativos1(){
+    
+    let popup = document.getElementById("popup1")
+    popup.style = "display: block";
+
+    let sair = document.getElementById("sair1")
+    sair.style = "display: block";
+
+    let ampliarFoto = document.getElementById("ampliar-foto1")
+    ampliarFoto.style = "display: block";
+
+    let overflow = document.getElementsByTagName("*");
+    for (var i = 0; i<overflow.length; i++) {
+        if (overflow[i].style.overflow = "hidden") {   
+        }
+    }
+
+};
+
+function sair1(){
+
+    let popup = document.getElementById("popup1")
+    popup.style = "display: none";
+
+    let sair = document.getElementById("sair1")
+    sair.style = "display: none";
+
+    let ampliarFoto = document.getElementById("ampliar-foto1")
+    ampliarFoto.style = "display: none";
+
+    let overflow = document.getElementsByTagName("*");
+    for (var i = 0; i<overflow.length; i++) {
+        if (overflow[i].style.overflow = "auto") {    
+        }
+    }
+
+};
+
+//popup 2
+
+function certificado_aplicativos2(){
+    
+    let popup = document.getElementById("popup2")
+    popup.style = "display: block";
+
+    let sair = document.getElementById("sair2")
+    sair.style = "display: block";
+
+    let ampliarFoto = document.getElementById("ampliar-foto2")
+    ampliarFoto.style = "display: block";
+
+    let overflow = document.getElementsByTagName("*");
+    for (var i = 0; i<overflow.length; i++) {
+        if (overflow[i].style.overflow = "hidden") {   
+        }
+    }
+
+};
+
+function sair2(){
+
+    let popup = document.getElementById("popup2")
+    popup.style = "display: none";
+
+    let sair = document.getElementById("sair2")
+    sair.style = "display: none";
+
+    let ampliarFoto = document.getElementById("ampliar-foto2")
+    ampliarFoto.style = "display: none";
+
+    let overflow = document.getElementsByTagName("*");
+    for (var i = 0; i<overflow.length; i++) {
+        if (overflow[i].style.overflow = "auto") {    
+        }
+    }
+
+};
